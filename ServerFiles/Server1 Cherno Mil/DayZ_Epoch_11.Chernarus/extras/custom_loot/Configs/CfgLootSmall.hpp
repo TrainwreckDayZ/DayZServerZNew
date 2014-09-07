7f7f344c79a5446995095f523ca3272507dcdd2d
Trainwreck <info@trainwreckdayz.com>
2014-09-06 21:01:27 -0500

    lates..

--- ServerFiles/Server1 Cherno Mil/DayZ_Epoch_11.Chernarus/extras/custom_loot/Configs/CfgLootSmall.hpp
+++ ServerFiles/Server1 Cherno Mil/DayZ_Epoch_11.Chernarus/extras/custom_loot/Configs/CfgLootSmall.hpp
@@ -1,88 +1,140 @@
 class CfgLootSmall {
 	trash[] = {
-		{"TrashTinCan",0.01}
+		{"TrashTinCan",0.02},
+		{"TrashJackDaniels",0.02},
+		{"ItemSodaEmpty",0.02},
+		{"ItemTrashToiletpaper",0.02},
+		{"ItemTrashRazor",0.02}
 	};
 	civilian[] = {
+		{"FoodCanUnlabeled",0.07},
+		{"TrashJackDaniels",0.09},
+		{"ItemSodaEmpty",0.09},
 		{"ItemSodaCoke",0.3},
 		{"ItemSodaPepsi",0.09},
 		{"FoodCanBakedBeans",0.05},
 		{"FoodCanSardines",0.05},
 		{"FoodCanFrankBeans",0.05},
 		{"FoodCanPasta",0.05},
-		{"8Rnd_9x18_Makarov",0.03},
+		{"8Rnd_9x18_Makarov",0.07},
+		{"7Rnd_45ACP_1911",0.05},
+		{"2Rnd_shotgun_74Slug",0.05},
+		{"2Rnd_shotgun_74Pellets",0.05},
 		{"ItemBandage",0.06},
 		{"ItemPainkiller",0.06},
 		{"FoodBioMeat",0.01},
 		{"8Rnd_9x18_MakarovSD",0.01}
 	};
 	office2[] = {
+		{"FoodCanUnlabeled",0.07},
+		{"TrashJackDaniels",0.09},
+		{"ItemSodaEmpty",0.09},
 		{"ItemSodaCoke",0.09},
 		{"ItemSodaPepsi",0.09},
 		{"FoodCanBakedBeans",0.05},
 		{"FoodCanSardines",0.05},
 		{"FoodCanFrankBeans",0.05},
 		{"FoodCanPasta",0.05},
-		{"8Rnd_9x18_Makarov",0.03},
+		{"8Rnd_9x18_Makarov",0.07},
+		{"7Rnd_45ACP_1911",0.05},
+		{"2Rnd_shotgun_74Slug",0.05},
+		{"2Rnd_shotgun_74Pellets",0.05},
 		{"ItemBandage",0.06},
 		{"ItemPainkiller",0.06},
 		{"FoodBioMeat",0.01},
 		{"8Rnd_9x18_MakarovSD",0.01}
 	};
 	food[] = {	
+		{"TrashTinCan",0.09},
+		{"TrashJackDaniels",0.12},
+		{"ItemSodaEmpty",0.12},
 		{"ItemSodaCoke",0.09},
 		{"ItemSodaPepsi",0.11},
 		{"FoodCanBakedBeans",0.07},
 		{"FoodCanSardines",0.07},
 		{"FoodCanFrankBeans",0.07},
 		{"FoodCanPasta",0.07},
+		{"FoodCanUnlabeled",0.3},
 		{"FoodPistachio",0.05},
 		{"FoodNutmix",0.04}
 	};
 	office[] = {
+		{"FoodCanUnlabeled",0.06},
+		{"ItemSodaEmpty",0.06},
 		{"ItemSodaCoke",0.06},
 		{"ItemSodaPepsi",0.04},
+		{"TrashJackDaniels",0.04},
 		{"FoodCanBakedBeans",0.01},
 		{"FoodCanSardines",0.01},
 		{"FoodCanFrankBeans",0.01},
 		{"FoodCanPasta",0.01},
+		{"ItemWaterbottleUnfilled",0.01},
 		{"ItemWaterbottle",0.01},
 		{"ItemBandage",0.08},
+		{"7Rnd_45ACP_1911",0.03},
 		{"5x_22_LR_17_HMR",0.01},
 		{"10x_303",0.04},
-		{"8Rnd_9x18_Makarov",0.04},
-		{"ItemPainkiller",0.02}
+		{"6Rnd_45ACP",0.04},
+		{"2Rnd_shotgun_74Slug",0.05},
+		{"2Rnd_shotgun_74Pellets",0.05},
+		{"8Rnd_9x18_Makarov",0.09},
+		{"15Rnd_W1866_Slug",0.02},
+		{"WoodenArrow",0.04},
+		{"HandRoadFlare",0.07},
+		{"ItemPainkiller",0.02},
+		{"HandChemGreen",0.01},
+		{"HandChemBlue",0.03},
+		{"HandChemRed",0.03},
+		{"ItemHeatPack",0.03}
 	};
 	generic[] = {
+		{"FoodCanUnlabeled",0.06},
+		{"ItemSodaEmpty",0.06},
 		{"ItemSodaCoke",0.06},
 		{"ItemSodaPepsi",0.04},
+		{"TrashJackDaniels",0.04},
 		{"FoodCanBakedBeans",0.01},
 		{"FoodCanSardines",0.01},
 		{"FoodCanFrankBeans",0.01},
 		{"FoodCanPasta",0.01},
+		{"ItemWaterbottleUnfilled",0.01},
 		{"ItemWaterbottle",0.01},
 		{"ItemBandage",0.11},
+		{"7Rnd_45ACP_1911",0.03},
 		{"5x_22_LR_17_HMR",0.01},
 		{"10x_303",0.04},
-		{"8Rnd_9x18_Makarov",0.04},
-		{"ItemPainkiller",0.02}
+		{"6Rnd_45ACP",0.04},
+		{"2Rnd_shotgun_74Slug",0.05},
+		{"2Rnd_shotgun_74Pellets",0.05},
+		{"8Rnd_9x18_Makarov",0.09},
+		{"15Rnd_W1866_Slug",0.02},
+		{"WoodenArrow",0.04},
+		{"HandRoadFlare",0.07},
+		{"ItemPainkiller",0.02},
+		{"HandChemGreen",0.01},
+		{"HandChemBlue",0.03},
+		{"HandChemRed",0.03},
+		{"ItemHeatPack",0.04}
 	};
 	medical[] = {
-		{"ItemBandage",0.25},
-		{"ItemPainkiller",0.12},
+		{"ItemBandage",0.4},
+		{"ItemPainkiller",0.15},
 		{"ItemMorphine",0.2},
-		{"ItemEpinephrine",0.1},
-		{"ItemAntibiotic",0.15},
-		{"ItemBloodbag",0.1}
+		{"ItemEpinephrine",0.3},
+		{"ItemAntibiotic",0.3},
+		{"ItemHeatPack",0.05}
 	};
 	hospital[] = {
-		{"ItemBandage",0.2},
-		{"ItemPainkiller",0.1},
-		{"ItemMorphine",0.15},
-		{"ItemEpinephrine",0.05},
-		{"ItemBloodbag",0.2},
-		{"ItemAntibiotic",0.07}
+		{"ItemBandage",0.33},
+		{"ItemPainkiller",0.16},
+		{"ItemMorphine",0.13},
+		{"ItemEpinephrine",0.09},
+		{"ItemBloodbag",0.17},
+		{"ItemAntibiotic",0.12}
 	};
 	military[] = {
+		{"FoodCanUnlabeled",0.09},
+		{"ItemSodaEmpty",0.04},
 		{"ItemSodaCoke",0.01},
 		{"ItemSodaPepsi",0.01},
 		{"ItemBandage",0.04},
@@ -98,28 +150,49 @@ class CfgLootSmall {
 		{"30Rnd_545x39_AK",0.04},
 		{"5Rnd_762x51_M24",0.01},
 		{"5Rnd_86x70_L115A1",0.01},
+		{"8Rnd_B_Beneli_74Slug",0.04},
 		{"1Rnd_HE_M203",0.01},
+		{"FlareWhite_M203",0.01},
+		{"FlareGreen_M203",0.01},
+		{"1Rnd_Smoke_M203",0.01},
 		{"200Rnd_556x45_M249",0.01},
 		{"HandGrenade_west",0.01},
 		{"HandGrenade_east",0.01},
-		{"SmokeShell",0.05},
+		{"SmokeShell",0.04},
+		{"SmokeShellRed",0.02},
+		{"SmokeShellGreen",0.02},
+		{"8Rnd_B_Beneli_Pellets",0.04},
 		{"30Rnd_556x45_StanagSD",0.01},
+		{"30Rnd_9x19_MP5",0.02},
 		{"30Rnd_9x19_MP5SD",0.01},
 		{"100Rnd_762x51_M240",0.01},
+		{"HandChemGreen",0.02},
+		{"HandChemBlue",0.02},
+		{"HandChemRed",0.02},
+		{"ItemHeatPack",0.04},
 		{"8Rnd_9x18_MakarovSD",0.01},
 		{"20Rnd_B_765x17_Ball",0.04},
 		{"30Rnd_9x19_UZI_SD",0.01}
 	};
 	policeman[] = {
 		{"ItemBandage",0.3},
+		{"7Rnd_45ACP_1911",0.08},
+		{"6Rnd_45ACP",0.08},
+		{"15Rnd_W1866_Slug",0.14},
+		{"8Rnd_B_Beneli_Pellets",0.17},
+		{"HandRoadFlare",0.08},
 		{"8Rnd_9x18_MakarovSD",0.01},
 		{"ItemComboLock",0.05},
+		{"SmokeShell",0.04},
 		{"ItemHotwireKit",0.01}
 	};
 	hunter[] = {
-		{"FoodMRE",0.1}
+		{"FoodMRE",0.01}
 	};
 	worker[] = {
+		{"FoodCanUnlabeled",0.09},
+		{"TrashJackDaniels",0.3},
+		{"ItemSodaEmpty",0.3},
 		{"ItemSodaCoke",0.11},
 		{"ItemSodaPepsi",0.3},
 		{"FoodCanBakedBeans",0.06},
@@ -132,11 +205,16 @@ class CfgLootSmall {
 		{"ItemSledgeHead",0.02}
 	};
 	clothes[] = {
-		{"Skin_RU_Policeman_DZ",0.15},
-		{"Skin_Pilot_EP1_DZ",0.2},
-		{"Skin_Functionary1_EP1_DZ",0.15},
-		{"Skin_Priest_DZ",0.1},
-		{"Skin_Rocker1_DZ",0.1},
+		{"Skin_RU_Policeman_DZ",0.13},
+		{"Skin_Pilot_EP1_DZ",0.12},
+		{"Skin_Functionary1_EP1_DZ",0.3},
+		{"Skin_Priest_DZ",0.3},
+		{"Skin_Rocker1_DZ",0.07},
+		{"Skin_Rocker2_DZ",0.07},
+		{"Skin_Rocker3_DZ",0.08},
+		{"Skin_Rocker4_DZ",0.08},
+		{"Skin_SurvivorW3_DZ",0.09},
+		{"Skin_SurvivorWpink_DZ",0.09},
 		{"Skin_SurvivorWurban_DZ",0.07}
 	};
 	militaryclothes[] = {
@@ -144,13 +222,27 @@ class CfgLootSmall {
 		{"Skin_Sniper1_DZ",0.08},
 		{"Skin_Rocket_DZ",0.05},
 		{"Skin_Soldier1_DZ",0.13},
+		{"Skin_Drake_Light_DZ",0.07},
+		{"Skin_Soldier_TL_PMC_DZ",0.07},
+		{"Skin_Soldier_Sniper_PMC_DZ",0.05},
+		{"Skin_Soldier_Bodyguard_AA12_PMC_DZ",0.07},
+		{"Skin_CZ_Special_Forces_GL_DES_EP1_DZ",0.08},
 		{"Skin_FR_OHara_DZ",0.08},
-		{"Skin_FR_Rodriguez_DZ",0.09}
+		{"Skin_FR_Rodriguez_DZ",0.09},
+		{"Skin_CZ_Soldier_Sniper_EP1_DZ",0.04},
+		{"Skin_Graves_Light_DZ",0.09}
 	};
 	specialclothes[] = {
 		{"Skin_Ins_Soldier_GL_DZ",0.3},
 		{"Skin_GUE_Commander_DZ",0.08},
+		{"Skin_Bandit1_DZ",0.13},
+		{"Skin_Bandit2_DZ",0.06},
+		{"Skin_BanditW1_DZ",0.07},
+		{"Skin_BanditW2_DZ",0.07},
+		{"Skin_TK_INS_Soldier_EP1_DZ",0.05},
+		{"Skin_TK_INS_Warlord_EP1_DZ",0.07},
 		{"Skin_SurvivorWcombat_DZ",0.08},
+		{"Skin_SurvivorWdesert_DZ",0.08},
 		{"Skin_GUE_Soldier_MG_DZ",0.05},
 		{"Skin_GUE_Soldier_Sniper_DZ",0.04},
 		{"Skin_GUE_Soldier_Crew_DZ",0.04},
@@ -166,9 +258,13 @@ class CfgLootSmall {
 		{"200Rnd_556x45_M249",0.14}
 	};
 	pistols[] = {
+		{"Makarov",0.25},
+		{"Colt1911",0.23},
+		{"revolver_EP1",0.20},
 		{"glock17_EP1",0.10},
 		{"MakarovSD",0.10},
 		{"M9",0.07},
-		{"M9SD",0.04}
+		{"M9SD",0.04},
+		{"revolver_gold_EP1",0.01}
 	};
 };
